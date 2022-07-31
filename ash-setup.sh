b839080b1b1d9417c8825e048f1786a384e9e446
Shanto Noor <shantonoor900@gmail.com>
2022-07-31 22:55:30 +0600

    Update

--- ash-setup.sh
+++ ash-setup.sh
@@ -1,7 +1,10 @@
 ln -s /data/data/com.termux/files/home/.bashsc ~/.profile
 ln -s /data/data/com.termux/files/home/.gitconfig ~/.gitconfig
+mkdir ~/.config
 ln -s /data/data/com.termux/files/home/.config/nvim/ ~/.config/nvim
 rm -rf ~/.local/share/nvim
+mkdir ~/.local
+mkdir ~/.local/share
 ln -s /data/data/com.termux/files/home/.local/share/nvim/ ~/.local/share/nvim
 ln -s /data/data/com.termux/files/home/.ssh/ ~/.ssh
 eval "$(ssh-agent -s)"

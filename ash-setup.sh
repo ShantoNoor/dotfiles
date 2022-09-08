0e08fc6f9d9b900664a723c3b64b1b768b37e709
Shanto Noor <shantonoor900@gmail.com>
2022-09-08 15:31:27 +0600

    Update

--- ash-setup.sh
+++ ash-setup.sh
@@ -1,15 +1,18 @@
-# ln -s /data/data/com.termux/files/home/.bashsc ~/.profile
-# ln -s /data/data/com.termux/files/home/.gitconfig ~/.gitconfig
-# mkdir ~/.config
-# ln -s /data/data/com.termux/files/home/.config/nvim/ ~/.config/nvim
-# rm -rf ~/.local/share/nvim
-# mkdir ~/.local
-# mkdir ~/.local/share
-# ln -s /data/data/com.termux/files/home/.local/share/nvim/ ~/.local/share/nvim
-# ln -s /data/data/com.termux/files/home/.ssh/ ~/.ssh
-# ln -s /data/data/com.termux/files/home/cp/ ~/cp
-# eval "$(ssh-agent -s)"
-# ssh-add ~/.ssh/id_ed25519
+if [[ "`uname -v`" == "#1 SMP PREEMPT Fri Jul 22 10:57:45 CST 2022" ]]
+then
+  ln -s /data/data/com.termux/files/home/.bashsc ~/.profile
+  ln -s /data/data/com.termux/files/home/.gitconfig ~/.gitconfig
+  mkdir ~/.config
+  ln -s /data/data/com.termux/files/home/.config/nvim/ ~/.config/nvim
+  rm -rf ~/.local/share/nvim
+  mkdir ~/.local
+  mkdir ~/.local/share
+  ln -s /data/data/com.termux/files/home/.local/share/nvim/ ~/.local/share/nvim
+  ln -s /data/data/com.termux/files/home/.ssh/ ~/.ssh
+  ln -s /data/data/com.termux/files/home/cp/ ~/cp
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_ed25519
+fi
 
 if [[ "`uname -v`" == "#1 SMP PREEMPT Fri Dec 10 13:53:37 WIB 2021" ]]
 then

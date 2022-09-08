395cf4ce94a4c0b558d95462c12ddde8d327e876
Shanto Noor <shantonoor900@gmail.com>
2022-09-08 09:26:36 +0000

    Update

--- .bashrc
+++ .bashrc
@@ -1,7 +1,6 @@
-
-if [[ $"uname -v" == "#1 SMP PREEMPT Fri Dec 10 13:53:37 WIB 2021" ]]
+if [[ "`uname -v`" == "#1 SMP PREEMPT Fri Dec 10 13:53:37 WIB 2021" ]]
 then
-  proot distro login alpine
+  proot-distro login alpine
   exit
 fi
 
--- ash-setup.sh
+++ ash-setup.sh
@@ -7,7 +7,14 @@
 # mkdir ~/.local/share
 # ln -s /data/data/com.termux/files/home/.local/share/nvim/ ~/.local/share/nvim
 # ln -s /data/data/com.termux/files/home/.ssh/ ~/.ssh
-ln -s /data/data/com.termux/files/home/cp/ ~/cp
+# ln -s /data/data/com.termux/files/home/cp/ ~/cp
 # eval "$(ssh-agent -s)"
 # ssh-add ~/.ssh/id_ed25519
+
+if [[ "`uname -v`" == "#1 SMP PREEMPT Fri Dec 10 13:53:37 WIB 2021" ]]
+then
+  ln -s ./.bashsc ./.profile
+  rm /data/data/com.termux/files/home/.bashrc
+  cp .bashrc /data/data/com.termux/files/home/.bashrc
+fi
 

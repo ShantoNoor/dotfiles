c5165801505cf14c1a99c73272f7f891100a804e
Shanto Noor <shantonoor900@gmail.com>
2025-08-11 03:07:57 +0600

    python lsp added

--- nvim/.config/nvim/lua/lspconfig.lua
+++ nvim/.config/nvim/lua/lspconfig.lua
@@ -20,7 +20,44 @@ vim.lsp.config("lua_ls", {
   },
 })
 
-vim.lsp.enable({"clangd", "lua_ls", "pylsp"})
+local venv_path = os.getenv('VIRTUAL_ENV')
+local py_path = nil
+-- decide which python executable to use for mypy
+if venv_path ~= nil then
+  py_path = venv_path .. "/bin/python"
+else
+  py_path = vim.g.python3_host_prog
+end
+vim.lsp.config("pylsp", {
+  settings = {
+    pylsp = {
+      plugins = {
+        -- formatter options
+        black = { enabled = false },
+        autopep8 = { enabled = false },
+        yapf = { enabled = false },
+        -- linter options
+        pylint = { enabled = false, executable = "pylint" },
+        ruff = { enabled = true, formatEnabled = true, extendSelect = { "I" }, },
+        pyflakes = { enabled = false },
+        pycodestyle = { enabled = false },
+        -- type checker
+        pylsp_mypy = {
+          enabled = true,
+          overrides = { "--python-executable", py_path, true },
+          report_progress = true,
+          live_mode = false
+        },
+        -- auto-completion options
+        jedi_completion = { fuzzy = true },
+        -- import sorting
+        isort = { enabled = true },
+      },
+    },
+  },
+})
+
+vim.lsp.enable({ "clangd", "lua_ls", "pylsp" })
 
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),

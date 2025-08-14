4dd21a2479d61418c304f302bbfca1a0dfd94dd8
Shanto Noor <shantonoor900@gmail.com>
2025-08-14 19:42:05 +0600

    update

--- nvim/.config/nvim/lua/lsp/clangd.lua
+++ nvim/.config/nvim/lua/lsp/clangd.lua
@@ -12,9 +12,5 @@ if vim.g.os_name == "Darwin" then
 	table.insert(cmd, "--query-driver=/opt/homebrew/bin/g++-*,/opt/homebrew/bin/gcc-*")
 end
 
-if vim.g.os_name == "Android" then
-	table.insert(cmd, "--query-driver=g++ -I" .. vim.g.cp_root)
-end
-
 vim.lsp.config("clangd", { cmd = cmd })
 vim.lsp.enable("clangd")
